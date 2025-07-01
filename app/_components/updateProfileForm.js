8e3ea891d850d0855cfa8746327f79b7401e750d
zakariaoudjdi <your.email@example.com>
2025-07-01 19:20:29 +0100

    fix: update .gitignore to allow .env files and format import statements in UpdateProfileForm component

--- app/_components/updateProfileForm.js
+++ app/_components/updateProfileForm.js
@@ -1,8 +1,7 @@
 "use client";
 
-import {useState} from "react";
+import { useState } from "react";
 function UpdateProfileForm({ children }) {
-
   const [country, setCountry] = useState("");
 
   return (
