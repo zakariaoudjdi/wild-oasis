7ed5ef80c5602d35bd4439af3e23f8984cee83aa
zakariaoudjdi <your.email@example.com>
2025-07-04 23:23:34 +0100

    feat: Update profile form to use useFormStatus for button state management

--- app/_components/updateProfileForm.js
+++ app/_components/updateProfileForm.js
@@ -2,11 +2,11 @@
 
 import { updateProfileAction } from "@/app/_lib/actions";
 import { useState } from "react";
-import { useFormState } from "react-dom";
+import { useFormStatus } from "react-dom";
 function UpdateProfileForm({ children, guest }) {
   const [country, setCountry] = useState("");
   const { fullName, email, nationality, nationalID, countryFlag } = guest || {};
-  const formState = useFormState();
+
   return (
     <form
       action={updateProfileAction}
@@ -41,20 +41,29 @@ function UpdateProfileForm({ children, guest }) {
       <div className="space-y-2">
         <label htmlFor="nationalID">National ID number</label>
         <input
-            name="nationalID"
-            defaultValue={nationalID}
-
+          name="nationalID"
+          defaultValue={nationalID}
           className="w-full rounded-sm bg-zinc-200 px-5 py-3 text-zinc-800 shadow-sm"
         />
       </div>
 
       <div className="flex items-center justify-end gap-6">
-        <button className="bg-yellow-500 hover:bg-yellow-600 px-8 py-4 font-semibold text-zinc-800 transition-all cursor-pointer disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300" >
-          {formState.submitting ? "Updating..." : "Update profile"}
-        </button>
+        <Button />
       </div>
     </form>
   );
 }
 
+function Button() {
+  const formState = useFormStatus();
+  return (
+    <button
+      type="submit"
+      disabled={formState.pending}
+      className="cursor-pointer bg-yellow-500 px-8 py-4 font-semibold text-zinc-800 transition-all hover:bg-yellow-600 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+    >
+      {formState.pending ? "Updating..." : "Update profile"}
+    </button>
+  );
+}
 export default UpdateProfileForm;
