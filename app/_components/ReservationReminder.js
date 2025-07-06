c50a54ec15fda389a44c3d40593ad20ab5ff50d8
zakariaoudjdi <zakariaoudjdi@gmail.com>
2025-07-06 19:23:52 +0100

    feat: Add date formatting to ReservationReminder for better user experience

--- app/_components/ReservationReminder.js
+++ app/_components/ReservationReminder.js
@@ -1,5 +1,6 @@
 "use client";
 import { XMarkIcon } from '@heroicons/react/24/solid';
+import { format } from 'date-fns';
 import { useReservations } from './ReservationsContext';
 
 function ReservationReminder() {
@@ -9,7 +10,11 @@ const {range,resetRange} = useReservations();
 
   return (
     <div className='fixed bottom-6 left-1/2 -translate-x-1/2 py-5 px-8 rounded-full bg-yellow-500 text-zinc-800 text  font-semibold shadow-xl shadow-slate-900 flex gap-8 items-center'>
-  
+      <p>
+        <span>👋</span> Don forget to reserve your dates <br /> from{''}
+        {format(new Date(range.from), 'MMM dd yyyy')} to{''}
+        {format(new Date(range.to), 'MMM dd yyyy')}
+      </p>
       <button onClick={resetRange} className='rounded-full p-1 hover:bg-yellow-600 transition-all'>
         <XMarkIcon className='h-5 w-5' />
       </button>
